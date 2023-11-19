d6f004a0e6e2a848a919c072f165283844b30042
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-19 19:03:19 +0600

    understood app and req object

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // app.use(express.json());
 app.use(express.raw());
+app.use(cookieParser());
 
 app.post('/', (req, res) => {
 	// console.log(req.body);
-	console.log(req.body.toString());
+	// console.log(req.body.toString());
+	// console.log(app.mountpath);
+	console.log(req.cookies);
 	res.send('Hello World!');
+});
+
+// application methods
+const admin = express();
+app.use('/admin', admin);
+
+admin.post('/', (req, res) => {
+	console.log(req.baseUrl);
+	res.send('Success');
 });
 
 app.listen(3000);
